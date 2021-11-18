0aecac93d2ee21f7a80bca73a332ae9361cde5fd
Crhistian <crhistiandeoliveira@gmail.com>
2021-11-18 20:59:26 -0300

    added search character comics by id

--- src/main.ts
+++ src/main.ts
@@ -175,13 +175,45 @@ app.get("/:id", async (req: Request, res: Response) => {
         console.log(spyApi(req, id));
 
         let character = apiResponse.data.data.results;
-        console.log(character);
+
         let bottomMessageHTML = `
     <a href=\"http://marvel.com\" class='text-center'>${apiResponse.data.attributionText}</a>`;
 
         return res.status(200).send({
             message: "ok",
             data: character,
+            copy: bottomMessageHTML,
+        });
+    } catch (err: any) {
+        return res.status(500).send({
+            message: err.toString(),
+        });
+    }
+});
+
+app.get("/comics/:id", async (req: Request, res: Response) => {
+    try {
+        const page: number = req.query.page ? Number(req.query.page as string) : 1;
+        const limit: number = req.query.limit ? Number(req.query.limit as string) : 10;
+        const offset = limit * (page - 1);
+        const id = req.params.id;
+        const apiResponse = await apiMarvel.get(`/characters/${id}`, {
+            params: {
+                ...createAuth(),
+                limit,
+                offset,
+            },
+        });
+        console.log(spyApi(req, id));
+
+        let comicsList = apiResponse.data.data.results;
+
+        let bottomMessageHTML = `
+    <a href=\"http://marvel.com\" class='text-center'>${apiResponse.data.attributionText}</a>`;
+
+        return res.status(200).send({
+            message: "ok",
+            data: comicsList,
             copy: bottomMessageHTML,
         });
     } catch (err: any) {
