73789fadda9a9a13bb820a016b4aa577cc64e230
Crhistian <crhistiandeoliveira@gmail.com>
2021-11-18 18:54:44 -0300

    minor fix?

--- src/main.ts
+++ src/main.ts
@@ -163,7 +163,7 @@ app.get("/", async (req: Request, res: Response) => {
         const page: number = req.query.page ? Number(req.query.page as string) : 1;
         const limit: number = req.query.limit ? Number(req.query.limit as string) : 10;
         const offset = limit * (page - 1);
-        const id = req.query.id ? (req.query.id as string) : undefined;
+        const id = req.params.id ? (req.params.id as string) : undefined;
         const apiResponse = await apiMarvel.get("/characters/", {
             params: {
                 ...createAuth(),
